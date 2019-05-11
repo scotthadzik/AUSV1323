173bcb09d9ef3eb0ef556cb41fb03b4649fa970b
Scott Hadzik <scotthadzik@weber.edu>
2019-05-10 20:03:58 -0600

    changed to board

--- servoCheck.py
+++ servoCheck.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 servoPIN = 31
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.Board)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
