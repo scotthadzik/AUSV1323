8d785d71cc59d24c196306c7f4052704a0d51ffc
Scott Hadzik <scotthadzik@weber.edu>
2019-07-03 20:13:45 -0600

    added test for relay class

--- Relay.py
+++ Relay.py
@@ -1,5 +1,16 @@
+
+
 class Relay:
+
+	import RPi.GPIO as GPIO
+	
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+
+
 	def __init__ (self, name, relayNumber, outputPin):
 		self.name 	= name
 		self.relayNumber = relayNumber
 		self.outputPin = outputPin
+
+	def toggleRelay(self):
+		GPIO.output(self.outputPin, GPIO.LOW)
---
+++ testRelayClass.py
@@ -0,0 +1,4 @@
+from Relay import Relay
+
+relayB1 = Relay("Relay B1", 9, 40)
+relayB1.toggleRelay
