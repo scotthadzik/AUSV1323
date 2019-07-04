b2d3e1a80d158ffa6004d3c9b1e68e3d2180928e
Scott Hadzik <scotthadzik@weber.edu>
2019-07-03 21:43:39 -0600

    added on call

--- testRelayClass.py
+++ testRelayClass.py
@@ -1,10 +1,26 @@
 import time
 from Relay import Relay
 from Lesson import Lesson
+import GPIO as GPIO
 
-relayB1 = Relay("Relay B1", 9, 40)
-print('relayOn')
-time.sleep(2)
+def setup(): 
+	relayB1 = Relay("Relay B1", 9, 40)
+	relayB1.turnRelayON
+	print('relayOn')
+	time.sleep(2)
 
-lesson1 = Lesson('lesson1', 1, relayB1)
-lesson1.showLessonInfo()
+	lesson1 = Lesson('lesson1', 1, relayB1)
+	lesson1.showLessonInfo()
+
+
+def destroy():
+	GPIO.cleanup()
+	pass
+
+if __name__ == "__main__":
+	try:
+		setup()
+		while True:
+			pass
+	except KeyboardInterrupt:
+		destroy()
