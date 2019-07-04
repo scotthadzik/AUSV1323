5b3d27ebf3c9e1d6685fc017b1c40fffaf719a7b
Scott Hadzik <scotthadzik@weber.edu>
2019-07-03 20:34:39 -0600

    moved import out of class

--- Relay.py
+++ Relay.py
@@ -1,16 +1,25 @@
-
+import time
+import RPi.GPIO as GPIO
 
 class Relay:
 
-	import RPi.GPIO as GPIO
+	
+	# 
 	
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 
 
 	def __init__ (self, name, relayNumber, outputPin):
+		
 		self.name 	= name
 		self.relayNumber = relayNumber
 		self.outputPin = outputPin
 
+	def setupRelayPinAsOutput(self):
+		time.sleep(.5)
+		GPIO.setup(self.outputPin, GPIO.OUT, initial=0)
+
 	def toggleRelay(self):
+		time.sleep(0.5)
 		GPIO.output(self.outputPin, GPIO.LOW)
+		
