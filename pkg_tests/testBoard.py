3a7ecf864a5e86a1c67dd6fe16e41bd27e83a084
Scott Hadzik <scotthadzik@weber.edu>
2020-01-20 22:05:16 -0700

    relay on off

--- pkg_tests/testBoard.py
+++ pkg_tests/testBoard.py
@@ -11,18 +11,21 @@ def setup(relayList):
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	LCD1602.init(0x27, 1)	# init(slave address, background light)
 	LCD1602.clear	# init(slave address, background light)
-	LCD1602.write(0, 0, 'Electrical')
-	LCD1602.write(1, 1, 'Trainer')
+	LCD1602.write(0, 0, 'Relay')
+	LCD1602.write(1, 1, 'Check')
 	time.sleep(2)
 	
 	for relay in relayList:
 		GPIO.setup(relay.outputPin, GPIO.OUT, initial=0)
-		LCD1602.write(0, 0, 'Relay Check')
+	
+	cycle_relays(relayList, True)
+	cycle_relays(relayList, False)
+
+def cycle_relays(relayList, state):
 	for relay in relayList:
 		LCD1602.write(1, 1, relay.name)
-		relay.setRelayStatus(True)
+		relay.setRelayStatus(state)
 		time.sleep(relayCheckTime)
 	
-
 	
 
