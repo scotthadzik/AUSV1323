8ea1b103538878791402a728eb08cb22fe97c5bc
Scott Hadzik <scotthadzik@weber.edu>
2020-01-20 22:54:13 -0700

    3 faults

--- pkg_setup/setup_faults.py
+++ pkg_setup/setup_faults.py
@@ -4,8 +4,9 @@ def create_fault_list():
 	fault_list = []
 
 	fault_dict = {
-		'open': {'fault_relay': True, 'short_relay' :False, 'short_relay' :False},
-		'open': {'fault_relay': True, 'short_relay' :False, 'short_relay' :False}
+		'open':      {'fault_relay': True, 'short_relay' :False, 'resist_relay' :False},
+		'short':     {'fault_relay': True, 'short_relay' :False, 'resist_relay' :True},
+		'hi_resist': {'fault_relay': True, 'short_relay' :True , 'resist_relay' :False}
 	}
 
 	open_circuit_fault = Fault('Open Circuit Fault', True, False, False)
