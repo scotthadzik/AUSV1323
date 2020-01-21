0e4af92c0dd68f36e3f5d3a49fd3a59fef381797
Scott Hadzik <scotthadzik@weber.edu>
2020-01-20 22:46:27 -0700

    dict change

--- pkg_setup/setup_faults.py
+++ pkg_setup/setup_faults.py
@@ -2,6 +2,11 @@ from pkg_setup.Fault import Fault
 
 def create_fault_list():
 	fault_list = []
+
+	fault_dict {
+		'open': Fault('Open Circuit Fault', True, False, False),
+	}
+
 	open_circuit_fault = Fault('Open Circuit Fault', True, False, False)
 	hi_res_circuit_fault = Fault('High Resistance Circuit Fault', True, False, True)
 	short_circuit_fault = Fault('Open Circuit Fault', True, True, False)
@@ -9,4 +14,4 @@ def create_fault_list():
 	fault_list.append(open_circuit_fault)
 	fault_list.append(hi_res_circuit_fault)
 	fault_list.append(short_circuit_fault)
-	return fault_list
+	return fault_dict
